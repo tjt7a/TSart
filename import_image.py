f3112fea542b9c6721f7687a9e241b3976770e1d
tjt7a <tjta@virginia.edu>
2013-11-02 18:27:37 -0400

    Checker pattern inversion

--- import_image.py
+++ import_image.py
@@ -3,7 +3,7 @@ import numpy
 
 # Read in image and convert to grayscale
 img = Image.open('rotunda.jpeg')
-img_2 = i.convert('L')    #convert to grayscale
+img_2 = img.convert('L')    #convert to grayscale
 array = numpy.asarray(img_2)
 
 # Give output array dimmensions
@@ -12,8 +12,11 @@ out_array = numpy.zeros(array.size).reshape((len(array), len(array[0])))
 # Iterate through 2d array and invert values
 for i in range(len(array)):
 	for j in range(len(array[0])):
-		out_array[i][j] = 1 - array[i][j]
+		if(((j > len(array[0])/2) and (i < len(array)/2)) or ((j < len(array[0])/2) and (i > len(array)/2))):		
+			out_array[i][j] = 1 - array[i][j]
+		else:
+			out_array[i][j] = array[i][j]
 
 # Convert array to image
-out_img = Image.fromarray(array.astype(numpy.uint8))
+out_img = Image.fromarray(out_array.astype(numpy.uint8))
 out_img.save('img2.png')
