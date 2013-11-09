47209072a1604c97d5886a8f5211781410f49a5a
tjt7a <tjta@virginia.edu>
2013-11-08 23:25:22 -0500

    Test Power Law

--- import_image.py
+++ import_image.py
@@ -7,6 +7,7 @@
 #
 
 import Image
+import ImageEnhance
 import numpy
 import sys
 import stipple
@@ -14,13 +15,14 @@ import drawing
 
 #Syntax:
 #./import_image.py <inputfile> <outputfile>
-if(len(sys.argv) != 3):
+if(len(sys.argv) != 4):
 	print "ERROR: provide input and output file names"
 	exit()
 
 # Set input and output file names
 in_filename = sys.argv[1] 
 out_filename = sys.argv[2]
+exponent = float(sys.argv[3]) #exponent from 0 ->. If < 1, sublinear, if > 1, superlinear
 
 # Catch incorrect input filename
 try:
@@ -30,18 +32,30 @@ except IOError:
 	exit()
 
 # Convert to grayscale and then to array
-img_2 = img.convert('L') 
+img_2 = img.convert('L')
+
 array = numpy.asarray(img_2)
+
+look_up_table = []
+for i in range(256):
+	look_up_table.append(255 * ((i)**exponent)/((255)**exponent))
 
 # Give output array dimmensions
 out_array = numpy.zeros(array.size).reshape((len(array), len(array[0])))
 
+for i in range(len(array)):
+	for j in range(len(array[0])):
+		out_array[i][j] =  look_up_table[array[i][j]]
+
+out_img = Image.fromarray(out_array.astype(numpy.uint8))
+out_img.save(out_filename)
+
 # Call stipple.py
 
-stipples = stipple.stipple(array, 0.005, 0, 256)
-print "blocks = ",stipples[1]
-print "recursions = ",stipples[2]
-print "stipple number = ",stipples[3]
-img_3 = Image.new('L', (len(array[0]), len(array)), "white")
-out_img = drawing.draw_stipples(img_3, stipples[0], 2)
-out_img.save(out_filename)
+#stipples = stipple.stipple(out_array, 0.05, 0, 25)
+#print "blocks = ",stipples[1]
+#print "recursions = ",stipples[2]
+#print "stipple number = ",stipples[3]
+#img_4 = Image.new('L', (len(array[0]), len(array)), "white")
+#out_img = drawing.draw_stipples(img_4, stipples[0], 2)
+#out_img.save(out_filename)
