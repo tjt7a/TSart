05a13c2181bc62e5ee86077e475fb776bc93c0f5
tjt7a <tjta@virginia.edu>
2013-11-10 14:09:47 -0500

    Added delaunay_graph function to graph.py

--- graph.py
+++ graph.py
@@ -8,18 +8,92 @@
 #
 
 from math import *
+from scipy.spatial import Delaunay
+import numpy as np
+
+# Returns the edges in the Delaunay triangulation of the nodes
+def delaunay_graph(nodes):
+	edges = []
+	points = np.array(nodes)
+	tri = Delaunay(points)
+	simplices = tri.simplices
+	triangles = points[simplices].tolist()
+	for triangle in triangles:
+		edges.append((tuple(triangle[0]), tuple(triangle[1]), hypot(triangle[0][1] - triangle[1][1], triangle[0][0] - triangle[1][0])))
+		edges.append((tuple(triangle[1]), tuple(triangle[2]), hypot(triangle[1][1] - triangle[2][1], triangle[1][0] - triangle[2][0])))
+		edges.append((tuple(triangle[2]), tuple(triangle[0]), hypot(triangle[2][1] - triangle[0][1], triangle[2][0] - triangle[0][0])))
+	return edges
 
 def full_graph(nodes):
 	edges = []
+	print "Time to get full graph!"
 	while(len(nodes) != 0):
 		current_node = nodes.pop(0)
 		current_y = current_node[0]
 		current_x = current_node[1]
 
 		for i in range(len(nodes)):
-			temp = nodes[i]
-			temp_y = temp[0]
-			temp_x = temp[1]
+			temp_node = nodes[i]
+			temp_y = temp_node[0]
+			temp_x = temp_node[1]
 			
-			edges.append((current_node, temp, hypot(current_x - temp_x, current_y - temp_y)))
-	return edges 
+			weight = hypot(current_x - temp_x, current_y - temp_y)
+			edges.append((current_node, temp_node, weight))
+	return edges # Return tuple of (node_1, node_2, distance)
+
+
+def min_span_tree(nodes):
+
+	# Edges that make up the min spanning tree
+	span_tree = []
+
+	# Make forest of trees (list of nodes) with single node per tree
+	forest = [[] for x in xrange(len(nodes))]
+	for i in range(len(nodes)):
+		forest[i].append(nodes[i])
+
+	print "Finished the forest"
+
+	# Get all edges in a fully connected graph of the nodes and sort edges
+	#edges = full_graph(nodes)
+	edges = delaunay_graph(nodes)
+	print "Got delaunay graph!"
+	sorted_edges = sorted(edges, key=lambda edge: edge[2]) 
+	
+	print "Sorted delaunay graph"
+	# Iterate through all edges (until we're done)
+	for edge in sorted_edges:
+
+		# We're done! All nodes in one tree
+		if len(forest) == 1:
+			break
+
+		node_a = edge[0]
+		node_b = edge[1]
+
+		a_index = -1
+		b_index = -1
+
+		# Go through forest and find which tree node a and b are located
+		for i in range(len(forest)):
+
+			tree = forest[i]
+
+			if node_a in tree:
+				a_index = i
+
+			if node_b in tree:
+				b_index = i
+
+			if (a_index != -1) and (b_index != -1):
+				break
+
+		# They're in different trees! Great; let's join them
+		if a_index != b_index:
+			forest[a_index] = forest[a_index] + forest[b_index]
+			forest.pop(b_index)
+			span_tree.append(edge)
+
+		# Else: They're already in the same tree; don't care
+
+	return span_tree
